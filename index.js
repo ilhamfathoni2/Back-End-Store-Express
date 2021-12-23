9c378d037ea11a7adc12424dd6b9a8194b89847a
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-12-23 13:00:50 +0700

    Hello Wolrd

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/", router);
-
 app.use("/uploads", express.static("uploads"));
+
+app.get("/", function (req, res) {
+  res.send({
+    message: "Hello World",
+  });
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
-  document.write(`App listening at http://localhost:${port}`);
 });
